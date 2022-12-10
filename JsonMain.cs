1b6f83424bd4cef6380175041d6bc5bc3a1df796
bindhu97 <binduvishwakarma97@gmail.com>
2022-12-10 13:17:26 +0530

    [Bindhu] Data Mangement of Groceries

---
+++ JsonMain.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONInventory
+{
+    internal class JsonMain
+    {
+        model Inventory = new model();
+        List<model> InventoryList = new List<model>();
+        public void convert(string jFilePath)
+        {
+            using (StreamReader reader = new StreamReader(jFilePath))
+            {
+                var json = reader.ReadToEnd();
+                var items = JsonConvert.DeserializeObject<List<model>>(json);
+                Console.WriteLine("Name Weight Price Total Value");
+                foreach (var item in items) 
+                {
+                    double value = item.price * item.weight;
+                Console.WriteLine(item.name + " "+ item.weight + " "+ item.price + " "+ value);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+JSONInventory.JsonMain json = new JSONInventory.JsonMain();
+json.convert("C:\\Users\\91973\\source\\repos\\JSONInventory\\JSONInventory\\Inventory.json");
