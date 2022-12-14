36c34d1fbe2ec02e59ebd6d98177e264688dd302
bindhu97 <binduvishwakarma97@gmail.com>
2022-12-14 22:52:57 +0530

    [Bindhu]Stock Account Management

---
+++ StockAccountManagement/Model.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAccountManagement
+{
+    public class Model
+    {
+        public string Name { get; set; }
+
+        public int NumberofShare { get; set; }
+
+        public int Price { get; set; }
+    }
+}
---
+++ StockAccountManagement/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace StockAccountManagement
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            StockAccountManagement.StockInventory json = new StockAccountManagement.StockInventory();
+            json.convert("C:\\Users\\91973\\source\\repos\\JSONInventory\\StockAccountManagement\\Stcok.json");
+        }
+    }
+}
